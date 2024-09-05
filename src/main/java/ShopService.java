61b37e03403a54717d229b12e59e2512551a1902
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-05 14:52:22 +0200

    Modified ShopService and added 2 Exceptions

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -7,16 +8,25 @@ public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
+//Modify the 'addOrder' method in the ShopService so that an exception is thrown if the product does not exist.
+
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
+            Product productToOrder = productRepo.getProductById(productId).orElseThrow(() -> new ProductNotFoundException(productId));
             products.add(productToOrder);
         }
+
+//    public Order addOrder(List<String> productIds) {
+//        List<Product> products = new ArrayList<>();
+//        for (String productId : productIds) {
+//            Product productToOrder = productRepo.getProductById(productId);
+//            if (productToOrder == null) {
+//                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+//                return null;
+//            }
+//            products.add(productToOrder);
+//        }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products);
 
@@ -31,6 +41,16 @@ public class ShopService {
         return orderRepo.getOrders().stream()
                 .filter(order -> order.status() == status)
                 .collect(Collectors.toList());
+    }
+
+    //Add an 'updateOrder' method in the ShopService that updates the Order based on an orderId and a new order status. Use the Lombok @With annotation for this.
+
+    public void updateOrder(String orderId, OrderStatus newStatus) { //"erzeuge eine public Methode ohne Rückgabewert namens updateOrder mit den Übergabeparametern orderId und newStatus"
+        Order orderToUpdate = Optional.ofNullable(orderRepo.getOrderById(orderId)).orElseThrow(() -> new OrderNotFoundException(orderId)); //"erstelle eine neue Order (orderToUpdate) indem
+        // du ein Optional erstellst (leer oder befüllt = .ofNullable) das sich die Info über die orderId aus dem orderRepo holt. Sollte die orderId nicht vorhanden sein, wird die Exception
+        // OrderNotFoundException geworfen. Sonst...
+        orderRepo.removeOrder(orderId);// ..."loesche die Order mit der Id orderId aus dem orderRepo und..."
+        orderRepo.addOrder(orderToUpdate.withStatus(newStatus));//"...erstelle eine neue Order (orderToUpdate) mit der Id orderId und dem neuen Status newStatus"
     }
 }
 
